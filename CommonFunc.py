95b791fbe3aa992b337719e3e72a3e983a0e2800
Alex <sascha.yagello@yandex.ru>
2024-10-14 18:20:08 +0300

    Update anomaly and eda file

--- CommonFunc.py
+++ CommonFunc.py
@@ -4,6 +4,8 @@ import numpy as np
 from matplotlib import colors
 import statistics
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.ensemble import IsolationForest
+
 
 
 def draw_must_box(axs, axs2, df, column_name):
@@ -89,3 +91,105 @@ def getRemains(y_test, y_pred):
     plt.ylabel('Остатки')
     plt.title('Остатки vs Предсказанные значения')
     plt.show()
+
+
+def totuple(a):
+    try:
+        return tuple(totuple(i) for i in a)
+    except TypeError:
+        return a
+
+def choseAnomalyNotAnomaly(isAnomaly: bool, arr: list, arr_y: list) -> list:
+    newlist=[]
+    
+    for i in range(0,len(arr)):
+        if arr_y[i] == 1 and isAnomaly==False:
+            newlist.append(arr[i])
+        if arr_y[i] == -1 and isAnomaly==True:
+            newlist.append(arr[i])
+    return newlist
+    
+
+def AnomalyOneCategoryVisualisation(xx, yy, name, conts):
+    new_X = []  
+    amount_contamination = 2.0
+    for x,y_ in zip(xx,yy):
+        new_X.append([x,y_])
+
+    fig, axs = plt.subplots(1,len(conts))
+    fig.set_figheight(4)
+    fig.set_figwidth(12)
+                    
+    for i in range(0, len(conts)):
+        clf = IsolationForest(contamination=conts[i], random_state=42) 
+        clf.fit(new_X)
+        y_pred = clf.predict(new_X)
+        
+        normal_points = choseAnomalyNotAnomaly(False ,new_X, y_pred)
+    
+        axs[i].scatter([i[0] for i in normal_points], [i[1] for i in normal_points], c='blue', s=20, edgecolor='k', label="Нормальные точки")
+        
+        anomalies = choseAnomalyNotAnomaly(True ,new_X, y_pred)
+        
+        axs[i].scatter([i[0] for i in anomalies], [i[1] for i in anomalies], c='red', s=20, edgecolor='k', label="Аномалии")
+        axs[i].title.set_text(name +" " + conts[i].__str__())
+        axs[i].legend()
+
+
+def QuartilesAnomaly(data, name):    
+    Q1 = np.percentile(data, 25)
+    Q3 = np.percentile(data, 75)
+    IQR = Q3 - Q1
+    
+    # Определение границ для выбросов
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    
+    # Поиск выбросов
+    outliers = np.where((data < lower_bound) | (data > upper_bound))
+    
+    # Визуализация данных и выбросов
+    plt.figure(figsize=(12, 7))
+    plt.plot(data, 'bo', label='Данные')
+    plt.plot(outliers[0], data[outliers], 'ro', label='Выбросы')
+    plt.axhline(Q1, color='orange', linestyle='dashed', linewidth=2, label='Q1 (Первый квартиль)')
+    plt.axhline(Q3, color='green', linestyle='dashed', linewidth=2, label='Q3 (Третий квартиль)')
+    plt.axhline(lower_bound, color='red', linestyle='dotted', linewidth=2, label='Нижняя граница')
+    plt.axhline(upper_bound, color='red', linestyle='dotted', linewidth=2, label='Верхняя граница')
+    
+    plt.title('метод квартилей / ' + name)
+    plt.xlabel('Индекс')
+    plt.ylabel(name)
+    plt.legend()
+    plt.show()
+    
+    # Вывод найденных выбросов
+    print("Найденные выбросы:", data[outliers])
+
+
+def ZScoreAnomaly(data, name):
+    mean = np.mean(data)
+    std_dev = np.std(data)
+    z_scores = [(x - mean) / std_dev for x in data]
+    # Определение порога для выбросов
+    threshold = 3
+    
+    # Поиск выбросов
+    outliers = np.where(np.abs(z_scores) > threshold)
+    
+    # Визуализация данных и выбросов
+    plt.figure(figsize=(12, 7))
+    plt.plot(data, 'bo', label='Данные')
+    plt.plot(outliers[0], data[outliers], 'ro', label='Выбросы')
+    plt.axhline(mean, color='g', linestyle='dashed', linewidth=2, label='Среднее значение')
+    plt.axhline(mean + threshold * std_dev, color='r', linestyle='dotted', linewidth=2, label='Порог выбросов')
+    plt.axhline(mean - threshold * std_dev, color='r', linestyle='dotted', linewidth=2)
+    
+    plt.title('Z-Score / ' + name)
+    plt.xlabel('Индекс')
+    plt.ylabel(name)
+    plt.legend()
+    plt.show()
+    
+    # Вывод найденных выбросов
+    print("Найденные выбросы:", data[outliers])
